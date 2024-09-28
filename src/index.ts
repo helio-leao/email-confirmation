607f5f42c55e5a13ab119f59b79c39dbbd45342d
Helio Leao <hmtleao@hotmail.com>
2024-09-28 18:18:36 -0300

    add types for user and verification

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+type User = {
+  id: String;
+  username: String;
+  password: String;
+  verified: Boolean;
+};
+
+type UserVerification = {
+  userId: String;
+  uniqueString: String;
+  createdAt: Date;
+  spiresAt: Date;
+};
+
+const users: User[] = [];
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("works");
+  res.json(users);
 });
 
 app.listen(3000, () => console.log("server runing"));
