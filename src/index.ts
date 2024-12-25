608f7d899536c3626daed4cec5a3a7951918a5c2
Helio Leao <hmtleao@hotmail.com>
2024-12-25 16:15:12 -0300

    chore: update .gitignore and enhance build scripts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from "express";
 import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(express.json());
 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Server runing on port ${process.env.PORT}...`)
-);
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}...`));
